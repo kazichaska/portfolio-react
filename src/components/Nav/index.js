b1ccb35927e0c4b14a53ec0444bc107ef924188c
Kazi Islam <kazichaska@gmail.com>
2022-04-21 21:39:08 -0500

    image paths

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './nav.css'
+import image from '../../assets/images/img_avatar.png';
+
 
 function Nav({ currentPage, handlePageChange }) {
 
@@ -13,51 +15,51 @@ function Nav({ currentPage, handlePageChange }) {
 
     return (
         <header className='nav nav-item'>
-        <h1>Kazi Islam</h1>
-        <img src="/images/img_avatar.png" alt="Avatar" className="avatar"></img>
-        <ul className="nav nav-tabs">
-            <li className="nav-items">
-            <a
-                href="#about"
-                onClick={() => handlePageChange('About')}
-                onMouseOver={changeColorHover}
-                onMouseLeave={resetColor}
-                className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-            >
-                About Me
-            </a>
-            </li>
-            <li className="nav-items">
-            <a
-                href="#portfolio"
-                onClick={() => handlePageChange('Portfolio')}
-                onMouseOver={changeColorHover}
-                onMouseLeave={resetColor}
-                className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-            >
-                Portfolio
-            </a>
-            </li>
-            <li className="nav-items">
-            <a
-                href="#contact"
-                onClick={() => handlePageChange('Contact')}
-                onMouseOver={changeColorHover}
-                onMouseLeave={resetColor}
-                className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-            >
-                Contact
-            </a>
-            </li>
-            <li className="nav-items">
-            <a
-                href="/resume/Resume.pdf"
-                className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-            >
-                Resume
-            </a>
-            </li>
-        </ul>
+            <h1>Kazi Islam</h1>
+            <img src={image} alt="Avatar" className="avatar"></img>
+            <ul className="nav nav-tabs">
+                <li className="nav-items">
+                    <a
+                        href="#about"
+                        onClick={() => handlePageChange('About')}
+                        onMouseOver={changeColorHover}
+                        onMouseLeave={resetColor}
+                        className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+                    >
+                        About Me
+                    </a>
+                </li>
+                <li className="nav-items">
+                    <a
+                        href="#portfolio"
+                        onClick={() => handlePageChange('Portfolio')}
+                        onMouseOver={changeColorHover}
+                        onMouseLeave={resetColor}
+                        className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+                    >
+                        Portfolio
+                    </a>
+                </li>
+                <li className="nav-items">
+                    <a
+                        href="#contact"
+                        onClick={() => handlePageChange('Contact')}
+                        onMouseOver={changeColorHover}
+                        onMouseLeave={resetColor}
+                        className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                    >
+                        Contact
+                    </a>
+                </li>
+                <li className="nav-items">
+                    <a
+                        href="resume/Resume.pdf"
+                        className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+                    >
+                        Resume
+                    </a>
+                </li>
+            </ul>
         </header>
     );
 }
